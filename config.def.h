28aec232733f882f1a21ff93008aedc3257bdaf2
Lalith Tadigadapa <jordanator@users.noreply.github.com>
2021-05-13 11:28:06 +0530

    Changes

--- config.def.h
+++ config.def.h
@@ -126,8 +126,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,      setlayout,      {0} }, 
 	{ MODKEY,			            XK_w,      spawn,		   SHCMD("$BROWSER") },
 	{ MODKEY,			            XK_r,      spawn,		   SHCMD("$TERMINAL -e ranger") },
-	{ MODKEY,			            XK_y,      spawn,		   SHCMD("flameshot gui -p ~/Pictures/screenshots") },
-	{ MODKEY|ShiftMask,			    XK_y,      spawn,		   SHCMD("flameshot full -p ~/Pictures/screenshots") },
+	{ MODKEY,			            XK_s,      spawn,		   SHCMD("flameshot gui -p ~/Pictures/screenshots") },
+	{ MODKEY|ShiftMask,			    XK_s,      spawn,		   SHCMD("flameshot full -p ~/Pictures/screenshots") },
 	{ MODKEY,			            XK_e,      spawn,		   SHCMD("betterlockscreen -l dimblur") },
 	{ MODKEY|ShiftMask,			    XK_v,      spawn,		   SHCMD("mpv /dev/video0") },
 	{ MODKEY,                       XK_semicolon,      spawn,  SHCMD("skippy-xd") },
@@ -167,13 +167,15 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("passmenu -l 10 -y 400 -x 720 -z 500") },
 	{ MODKEY|ShiftMask,             XK_o,      spawn,          SHCMD("otpmenu -l 10 -y 400 -x 720 -z 500") },
+	{ MODKEY|ShiftMask|ControlMask, XK_p,      spawn,          SHCMD("powermenu") },
 	{ MODKEY|ShiftMask,             XK_x,      spawn,          SHCMD("killprog") },
 	{ MODKEY,                       XK_F1,      spawn,          SHCMD("msi-rgb-switch") },
 	{ MODKEY,                       XK_F2,      spawn,          SHCMD("screenkey -s small --scr 1 -p fixed -g 200x50+1+21 --opacity .8 --font-color white") },
 	{ MODKEY,                       XK_F3,      spawn,          SHCMD("killall screenkey") },
-	{ MODKEY|ShiftMask,             XK_r,      spawn,          SHCMD("reboot") },
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("shutdown -h now") },
-	{ MODKEY|ShiftMask|ControlMask, XK_y,      spawn,          SHCMD("ytfzf -D") },
+	/* { MODKEY|ShiftMask,             XK_r,      spawn,          SHCMD("reboot") }, */
+	/* { MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("shutdown -h now") }, */
+	{ MODKEY,                       XK_y,      spawn,          SHCMD("ytfzf -D") },
+	{ MODKEY|ShiftMask,             XK_y,      spawn,          SHCMD("ytfzf -DS") },
 	/* { MODKEY|ShiftMask,			    XK_t,      spawn,		   SHCMD("kitty -e ytop") }, */
     /* { MODKEY|ShiftMask,             XK_y,      setlayout,      {.v = &layouts[3]} }, // spiral */
 	/* { MODKEY,                       XK_0,      view,           {.ui = ~0 } }, */
