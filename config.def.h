f192bb6ca84d8550b6cb7064e1888a514df27be8
Lalith Tadigadapa <jordanator@users.noreply.github.com>
2021-12-05 10:13:20 +0530

    change

--- config.def.h
+++ config.def.h
@@ -13,12 +13,12 @@ static const int systraypinningfailfirst =
     1; /* 1: if pinning fails, display systray on the first monitor, False:
           display systray on the last monitor*/
 static const int showsystray = 1;      /* 0 means no systray */
-static const unsigned int gappih = 18; /* horiz inner gap between windows */
-static const unsigned int gappiv = 18; /* vert inner gap between windows */
+static const unsigned int gappih = 8; /* horiz inner gap between windows */
+static const unsigned int gappiv = 8; /* vert inner gap between windows */
 static const unsigned int gappoh =
-    18; /* horiz outer gap between windows and screen edge */
+    12; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
-    18; /* vert outer gap between windows and screen edge */
+    12; /* vert outer gap between windows and screen edge */
 static int smartgaps =
     0; /* 1 means no outer gap when there is only one window */
 static const int showbar = 1; /* 0 means no bar */
